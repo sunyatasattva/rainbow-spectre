b7415a5f5dac61862de906d0fc2a588f28362ea6
Veren <sunyatasattva@gmail.com>
2020-10-29 21:16:50 +0100

    Add modify base frequency option. Closes #5

--- src/components/OptionsBox.tsx
+++ src/components/OptionsBox.tsx
@@ -16,6 +16,15 @@ export default function OptionsBox() {
 
   return (
     <div className="options-container">
+      <div className="input-container">
+        <label htmlFor="reference-frequency">Reference frequency</label>
+        <input 
+          id="reference-frequency"
+          type="number"
+          onChange={(e) => setOption("baseFrequency", +e.target.value)}
+          value={options.baseFrequency}
+        />
+      </div>
       <Option onChange={setOption} label="Autoplay" value={options.autoplay} />
       <Option onChange={setOption} label="Lock ratio" value={options.lockRatio} />
     </div>
